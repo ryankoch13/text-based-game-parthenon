5cc68236a74d82a89408d28bde48359e877b3134
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2020-07-23 13:33:44 -0700

    opening page done

---
+++ parthenon/gameplay1.rb
@@ -0,0 +1,65 @@
+# Preamble
+
+puts "- Earth is in shambles! "
+puts
+sleep (2.1)
+puts "- After centuries of nuclear war and pollution Earth has become an inhospitable wasteland "
+puts
+sleep (4.5)
+puts "- The once vast human population that spread across the far reaches of the planet has now dwindled to to a mere 30,000 survivors, all grouped into Earth's last city, New Zanzibar"
+puts
+sleep (7.0)
+puts "- New Zanazibar, with it's dwindling resources, will not last for long"
+puts
+sleep (5.8)
+puts "- In a last ditch effort for survival the remaining human population has come together to build humanity's last hope; the Archships"
+puts
+sleep (6.9)
+puts "- 3 Archships, 'The Argonaught', 'The Akropolis',  and 'The Parthenon' are created to transport the entire human race to their new home"
+puts
+sleep (6.9)
+puts "- As one of New Zanzibars leading counsel members, you are charged with the duty of commanding one of these Archships"
+puts
+sleep (5.9)
+puts "- 'The Parthenon' is in your charge. Best of luck captain, the fate of humanity is in your hands"
+
+sleep (3.0)
+puts 
+puts 
+puts
+sleep (2.0)
+puts 
+puts 
+puts
+sleep (2.0)
+puts 
+puts 
+puts "
+       !
+       !
+       ^
+      / \\
+     /___\\
+    |=   =|
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+   /|##!##|\\
+  / |##!##| \\
+ /  |##!##|  \\
+|  / ^ | ^ \\  |
+| /  ( | )  \\ |
+|/   ( | )   \\|
+    ((   ))
+   ((  :  ))
+   ((  :  ))
+    ((   ))
+     (( ))
+      ( )"
+      
